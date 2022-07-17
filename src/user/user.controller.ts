98184fbc0b5febb6834fb000b497e30ab779e92a
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-07-17 16:09:21 +0300

    feat: implement updateUser endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,11 +6,14 @@ import {
   Body,
   Delete,
   HttpCode,
+  Put,
+  HttpStatus,
 } from '@nestjs/common';
 import { ParseUUIDPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { IUser, TUserId } from './user.model';
 import CreateUserDto from './dtos/createUser.dto';
+import UpdatePasswordDto from './dtos/updatePassword.dto';
 
 @Controller('user')
 export class UserController {
@@ -31,8 +34,16 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
+  @Put(':id')
+  update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: TUserId,
+    @Body() updateUserDto: UpdatePasswordDto,
+  ) {
+    return this.userService.update(id, updateUserDto);
+  }
+
   @Delete(':id')
-  @HttpCode(204)
+  @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id', new ParseUUIDPipe({ version: '4' })) id: TUserId) {
     return this.userService.delete(id);
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { IUser, TUserId } from './user.model';
 import database from 'src/db/db.service';
 import { v4 as uuid } from 'uuid';
 import CreateUserDto from './dtos/createUser.dto';
+import UpdatePasswordDto from './dtos/updatePassword.dto';
 
 @Injectable()
 export class UserService {
@@ -35,10 +41,33 @@ export class UserService {
   delete(id: TUserId): string {
     const newUsersArr = database.users.filter((item: IUser) => item.id !== id);
 
-    if (newUsersArr.length === database.users.length) {
+    if (
+      !database.users.length ||
+      newUsersArr.length === database.users.length
+    ) {
       throw new NotFoundException(`User with id: ${id} not found`);
     }
 
     return `User with id: ${id} was deleted!`;
+  }
+
+  update(id: TUserId, updatePasswordDto: UpdatePasswordDto): IUser {
+    const user = database.users.find((item: IUser) => item.id === id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const { newPassword, oldPassword } = updatePasswordDto;
+
+    if (oldPassword !== user.password) {
+      throw new HttpException('Old password incorrect', HttpStatus.FORBIDDEN);
+    }
+
+    user.password = newPassword;
+    user.version += 1;
+    user.updatedAt = Date.now();
+
+    return { ...user, password: '********' };
   }
 }
