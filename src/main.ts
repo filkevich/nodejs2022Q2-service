7662a10f0777f0b6c6525c286ccc05f1a3a300ae
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-07-21 18:44:42 +0300

    feat: connect swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = await readFile(
+    join(__dirname, '..', 'doc/api.yaml'),
+    'utf-8',
+  );
+
+  SwaggerModule.setup('docs', app, parse(document));
+
   await app.listen(process.env.PORT || 4000);
 }
 
